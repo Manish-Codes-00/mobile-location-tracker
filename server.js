52aefec7edf2d179772b4dabe96055cc87e05b25
Manish-Codes-00 <manishram0710@gmail.com>
2025-08-30 19:29:24 +0530

    backend updated to deploy on Render

--- server.js
+++ server.js
@@ -65,5 +65,7 @@ app.get("/api/lookup", async (req, res) => {
   }
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
